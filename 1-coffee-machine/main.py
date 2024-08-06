70ba4e359cadcca22e0b27b4ed4f75a560a567ae
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-08-05 23:51:06 -0700

    1. Start building out the functions that check if we have enough ingredients and how many we have left

--- 1-coffee-machine/main.py
+++ 1-coffee-machine/main.py
@@ -1,3 +1,4 @@
+# Define the menu items, how much they cost, and how much of each ingredient they take. This is best done in a dictionary
 MENU = {
     "espresso": {
         "ingredients": {
@@ -25,4 +26,32 @@ MENU = {
     }
 }
 
-print("hi")
+# Define the amount of resources we have to use. These numbers will change, hence the name is in all lowercase
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
+
+# Checks if there is enough resources to make the desired drink
+def check_ingredients(drink, resources):
+    # If the value of water needed to make the drink is greater than our water resource, show that as an alert. Do that for all ingredients
+    if drink["ingredients"]["water"] > resources["water"]:
+        print("Sorry, there is not enough water.")
+    elif drink["ingredients"]["coffee"] > resources["coffee"]:
+        print("Sorry, there is not enough coffee.")
+    elif drink["ingredients"]["milk"] > resources["milk"]:
+        print("Sorry, there is not enough milk.")
+    else:
+        # If we have enough of all ingredients, no error is displayed and the transaction continues to the next step
+        return True
+    
+# Format resources into printable format
+def format_resources(resources):
+    # Because resource amounts change, by calling the resources['water'], this should dynamically render the remaining ingredients
+    water_amount = resources["water"]
+    milk_amount = resources["milk"]
+    coffee_amount = resources["coffee"]
+    money_amount = resources["money"]
+    return f"Water: {water_amount}ml \nMilk: {milk_amount}ml \nCoffee: {coffee_amount}g \nMoney: ${money_amount}"
