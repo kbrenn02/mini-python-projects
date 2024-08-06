6719072811b2407809f4d16e5e86ad7895322f73
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-08-05 23:58:38 -0700

    1. Start the ordering process, using some of the functions I defined

--- 1-coffee-machine/main.py
+++ 1-coffee-machine/main.py
@@ -26,6 +26,7 @@ MENU = {
     }
 }
 
+
 # Define the amount of resources we have to use. These numbers will change, hence the name is in all lowercase
 resources = {
     "water": 300,
@@ -33,6 +34,7 @@ resources = {
     "coffee": 100,
     "money": 0
 }
+
 
 # Checks if there is enough resources to make the desired drink
 def check_ingredients(drink, resources):
@@ -47,6 +49,7 @@ def check_ingredients(drink, resources):
         # If we have enough of all ingredients, no error is displayed and the transaction continues to the next step
         return True
     
+
 # Format resources into printable format
 def format_resources(resources):
     # Because resource amounts change, by calling the resources['water'], this should dynamically render the remaining ingredients
@@ -55,3 +58,38 @@ def format_resources(resources):
     coffee_amount = resources["coffee"]
     money_amount = resources["money"]
     return f"Water: {water_amount}ml \nMilk: {milk_amount}ml \nCoffee: {coffee_amount}g \nMoney: ${money_amount}"
+
+
+# Returns the total from coins inserted
+def process_coins(q, d, n, p):
+    # Totals up how much the customer paid
+    return (q * .25) + (d * .10) + (n * .05) + (p * .01)
+
+
+# Create a while loop so the user can keep ordering until they say goodbye
+still_ordering = True
+while still_ordering:
+    # Take the customer's original input and put it in a consistent format
+    order = input("What would you like? (espresso/latte/cappuccino) or say goodbye: ").lower()
+
+    # Immediately end the loop if the customer leaves (this is also the first thing that happens after they finish ordering
+    # the first drink because of the loop)
+    if order == "off" or order == "goodbye" or order == "good bye" or order == "good-bye":
+        still_ordering = False
+
+    # This is a command for the employee side to check how many ingredients are left
+    if order == "report":
+        print(format_resources(resources))
+
+    # If the customer orders a valid drink, define a variable for that drink that we can work with and access from the Menu
+    if order == "espresso" or order == "latte" or order == "cappuccino":
+        drink = MENU[order]
+
+        # Check if we have enough ingredients to make the drink. It will return true or false. If true, the next block runs
+        checking = check_ingredients(drink, resources)
+        if checking:
+            # Reduce the resources dictionary by the amount used to make the drink. This nesting is how the ingredients for each
+            # drink is accessed in a nested dictionary
+            resources["water"] -= drink["ingredients"]["water"]
+            resources["coffee"] -= drink["ingredients"]["coffee"]
+            resources["milk"] -= drink["ingredients"]["milk"]
