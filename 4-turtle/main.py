643acdd647ce7ce9a44427fa2abd355edb0512aa
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-08-07 21:02:26 -0700

    4. Simple turtle art

--- 4-turtle/main.py
+++ 4-turtle/main.py
@@ -9,7 +9,39 @@
 
 import random
 import turtle
+# Need to pull specifically the Turtle and Screen classes from the turtle library
 from turtle import Turtle, Screen
+
+# Initiate the first turtle. Choose its color, shape, pensize, and speed
 tim = Turtle()
 turtle.colormode((255))
 tim.shape("turtle")
+directions = [0, 90, 180, 270]
+tim.pensize(1)
+tim.speed("fastest")
+
+
+# To get the turtle to do anything but exist we should create some functions. Random color generator!
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(size_of_gap):
+    # The _ means that the loop variable is not needed, and will just run this 360/X times
+    for _ in range(int(360/size_of_gap)):
+        # Call the random_color function. Circle(x) is the radius of the circle. Setheading is the way the turtle is facing
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+# Call the function
+draw_spirograph(10)
+
+
+# This shows the screen and doesn't let the screen disappear until it's clicked. This is really important to have when
+# using the turtle library
+screen = Screen()
+screen.exitonclick()
